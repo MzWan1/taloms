6238cfcddd513e7dd72e1c16ee989546bdf99d04
MzWan1 <208254049@tut4life.ac.za>
2026-07-03 20:18:58 +0200

    first commit

---
+++ src/test/java/za/co/taloms/TalomsApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.taloms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
