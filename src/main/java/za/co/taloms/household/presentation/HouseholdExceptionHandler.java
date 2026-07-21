f366b73f14a0f2a53afdac4f9e944bc89e40d733
MzWan1 <208254049@tut4life.ac.za>
2026-07-21 15:48:48 +0200

    fix: household module - add HIGHEST_PRECEDENCE ControllerAdvice to bypass GlobalExceptionHandler

--- src/main/java/za/co/taloms/household/presentation/HouseholdExceptionHandler.java
+++ src/main/java/za/co/taloms/household/presentation/HouseholdExceptionHandler.java
@@ -1,17 +1,20 @@
 package za.co.taloms.household.presentation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class HouseholdExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler(Throwable.class)
     @ResponseBody
-    public String handleException(Exception ex) {
+    public String handleException(Throwable ex) {
         log.error("Household page error: {}", ex.getMessage(), ex);
         return "Error loading household page: " + ex.getClass().getSimpleName() + " - " + ex.getMessage();
     }
